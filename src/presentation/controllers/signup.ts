0fc659ac50debf629755a6bc250f94934a2f7cb6
Markley da Silva Mendes <markley.mendes@gmail.com>
2022-09-27 07:19:59 -0300

    test: ensure SignUpController calls EmailValidator with correct email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,4 +1,4 @@
-import { InvalidParamError } from './../errors/invalid-param-error';
+import { InvalidParamError } from './../errors/invalid-param-error'
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helper/http-helper'
