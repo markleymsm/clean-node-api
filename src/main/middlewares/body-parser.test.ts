8914f7014b7cbc890fc1238be106e6a6e02cca90
Markley da Silva Mendes <markley.mendes@gmail.com>
2022-10-25 07:49:39 -0300

    feat: ensure requests parser body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body Parser Middleware', () => {
+  test('Should parser body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app).post('/test_body_parser').send({ name: 'Markley' }).expect({ name: 'Markley' })
+  })
+})
