274486513b22b468d3f888a39e72ce24f5c9c420
Markley da Silva Mendes <markley.mendes@gmail.com>
2022-10-27 07:19:15 -0300

    feat: ensure requests return json as content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -4,8 +4,8 @@ import app from '../config/app'
 describe('CORS Middleware', () => {
   test('Should CORS', async () => {
     app.get('/test_cors', (req, res) => {
-      res.send(req.body)
+      res.send()
     })
-    await request(app).get('/test_body_parser').expect('access-control-allow-origin', '*').expect('access-control-allow-methods', '*').expect('access-control-allow-headers', '*')
+    await request(app).get('/test_cors').expect('access-control-allow-origin', '*').expect('access-control-allow-methods', '*').expect('access-control-allow-headers', '*')
   })
 })
