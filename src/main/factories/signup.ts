8102c935ddde0b1f1db5be8881168d5df73eebd2
Markley da Silva Mendes <markley.mendes@gmail.com>
2022-11-16 07:52:58 -0300

    feat: add class Decorator

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,29 @@ import { AccountMongoRepository } from './../../infra/db/mongodb/account-reposit
 import { BcryptAdapter } from './../../infra/criptography/bcrypt-adapter'
 import { EmailValidatorAdapter } from './../../utils/email-validator-adapter'
 import { SignUpController } from './../../presentation/controllers/signup/signup'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 
-export const makeSignUpController = (): SignUpController => {
+class LofControllerDecorator implements Controller {
+  private readonly controller: Controller
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest)
+    if (httpResponse.statusCode === 500) {
+
+    }
+    return httpResponse
+  }
+}
+
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return new LofControllerDecorator(signUpController)
 }
